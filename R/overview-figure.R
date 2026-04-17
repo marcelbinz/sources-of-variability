d4430cf2d723bf7ea40adc149ebe3b71063e441c
MirkoTh <mirkothalmann@hotmail.com>
2026-04-17 13:28:47 +0200

    change coloring of tables

--- R/overview-figure.R
+++ R/overview-figure.R
@@ -292,7 +292,7 @@ htmltools::browsable(
 
 # Level 2 Shuffling -------------------------------------------------------
 
-tbl_shared_nohist_l2 <- tbl_shared_nohist %>%
+tbl_original_l2 <- tbl_original %>%
   select(-color) %>%
   mutate(is_swap = sample(rep(c(TRUE, FALSE), nrow(.)/2), size = nrow(.), replace = FALSE)) %>%
   rowwise() %>% mutate(V1_new = ifelse(is_swap, V2, V1), V2 = ifelse(is_swap, V1, V2), V1 = V1_new) %>% select(-V1_new)
@@ -300,43 +300,100 @@ tbl_shared_nohist_l2 <- tbl_shared_nohist %>%
 
 
 # tbl unshuffled between cols as base
-colors <- RColorBrewer::brewer.pal(n = 1, "Set2")
-tbl_shared_nohist$color <- colors[1]
+colors <- c(
+  RColorBrewer::brewer.pal(nrow(tbl_original), "Greens")[c(4)],
+  RColorBrewer::brewer.pal(nrow(tbl_original), "Reds")[c(4)]
+)
+tbl_original$color <- colors[1]
 
-datatable(tbl_shared_nohist, options = list(
-  columnDefs = list(
-    list(visible = FALSE, targets = c(ncol(tbl_shared_nohist)))  # hide last column
-  ), pageLength = 12,
-  rowCallback = JS(
-    "function(row, data) {
+dt.l2.1 <- datatable(
+  tbl_original,
+  colnames = c("ID", "Trial", "X1", "X2", "y", "color"),
+  rownames = FALSE,
+  options = list(
+    paging = FALSE,
+    scrollY = FALSE,
+    autoHeight = TRUE,
+    info = FALSE,
+    searching = FALSE,
+    dom = 't',
+    columnDefs = list(
+      list(visible = FALSE, targets = c(ncol(tbl_original)-1))  # hide last column
+    ), pageLength = 12,
+    rowCallback = JS(
+      "function(row, data) {
      $('td', row).css('background-color', data[data.length - 1]);
    }"
-  ))) %>%
-  formatStyle(
-    columns = c("V1", "V2"),
-    border = "2px solid blue"
-  )
+    )
+  )) %>%
+  my_formatting()
 
 
 # then tbl shuffled between cols
 
-colors <- RColorBrewer::brewer.pal(n = length(unique(tbl_shared_nohist_l2$is_swap)), "Set2")
-tbl_shared_nohist_l2$color <- colors[as.numeric(tbl_shared_nohist_l2$is_swap) + 1]
+tbl_original_l2$color <- colors[as.numeric(tbl_original_l2$is_swap) + 1]
+tbl_original_l2$color2 <- colors[1]
 
-datatable(tbl_shared_nohist_l2, options = list(
-  columnDefs = list(
-    list(visible = FALSE, targets = c((ncol(tbl_shared_nohist_l2)-1):ncol(tbl_shared_nohist_l2)))  # hide last column
-  ), pageLength = 12,
-  rowCallback = JS(
-    "function(row, data) {
-     $('td', row).css('background-color', data[data.length - 1]);
-   }"
-  ))) %>%
-  formatStyle(
-    columns = c("V1", "V2"),
-    border = "2px solid blue"
+dt.l2.2 <- datatable(tbl_original_l2,
+          rownames = FALSE,
+          colnames = c("ID", "Trial", "X1", "X2", "y", "is_swap", "color", "color2"),
+          options = list(
+            paging = FALSE,
+            scrollY = FALSE,
+            autoHeight = TRUE,
+            info = FALSE,
+            searching = FALSE,
+            dom = 't',
+            columnDefs = list(
+              list(visible = FALSE, targets = c((ncol(tbl_original_l2)-3):(ncol(tbl_original_l2))))  # hide last column
+            ), pageLength = 12,
+            rowCallback = JS(
+              "function(row, data) {
+         var color = data[data.length - 2];   // last column = color
+         var color2 = data[data.length - 1];
+
+         // color only column 3 (index 2)
+         $('td:eq(2)', row).css('background-color', color);
+
+         // color only column 5 (index 4)
+         $('td:eq(3)', row).css('background-color', color);
+         
+         // color only column 3 (index 2)
+         $('td:eq(0)', row).css('background-color', color2);
+         
+         // color only column 3 (index 2)
+         $('td:eq(1)', row).css('background-color', color2);
+         
+         // color only column 3 (index 2)
+         $('td:eq(4)', row).css('background-color', color2);
+       }"
+              )
+            )) %>%
+  my_formatting()
+
+
+
+htmltools::browsable(
+  tags$div(
+    style = "
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto;
+      column-gap: 40px;
+      align-items: start;
+    ",
+    
+    tags$div(
+      style = "grid-column: 1; grid-row: 1; height: 375px; width:350px",
+      dt.l2.1
+    ),
+    
+    tags$div(
+      style = "grid-column: 2; grid-row: 1; height: 375px; width:350px",
+      dt.l2.2
+    )
   )
-
+)
 
 
 # Level 1 Example Effects -------------------------------------------------
@@ -345,6 +402,8 @@ pd <- position_dodge(width = .1)
 plot_example_conditions <- function(my_tbl, ttl, plot_legend = FALSE) {
   idx_legend <- 1
   if (plot_legend) idx_legend <- 2
+  library(stringr)
+  
   ggplot(my_tbl, aes(History, y, group = ID)) +
     geom_hline(yintercept = .5, linetype = "dotdash", linewidth = 1, color = "red", alpha = .3) +
     geom_line((aes(color = ID)), position = pd) +
@@ -352,7 +411,10 @@ plot_example_conditions <- function(my_tbl, ttl, plot_legend = FALSE) {
     geom_point(aes(color = ID), size = 3, position = pd) +
     scale_color_brewer(palette = "Set1") +
     scale_y_continuous(expand = expansion(add = c(.02, 0))) +
-    labs(y = "Test Accuracy", x = element_blank(), title = ttl) +
+    labs(y = "Accuracy", x = element_blank(), title = ttl) +
+    scale_x_discrete(
+      labels = function(x) str_replace(x, "^(.{8})(.*)$", "\\1\n\\2")
+    ) +
     theme_bw() +
     theme(
       axis.title = element_text(size = 14),
@@ -365,11 +427,12 @@ plot_example_conditions <- function(my_tbl, ttl, plot_legend = FALSE) {
       title = element_text(size = 14)
     ) +
     coord_cartesian(ylim = c(.5, 1))
+  
 }
 
 
-v_history <- factor(levels = c("No History", "History"), ordered = TRUE)
-v_ID <- factor(levels = c("Shared", "ID"), ordered = TRUE)
+v_history <- factor(levels = c("Shuffled History", "Original History"), ordered = TRUE)
+v_ID <- factor(levels = c("Shuffled ID", "Original ID"), ordered = TRUE)
 tbl_main_id <- crossing(History = v_history, ID = v_ID) %>%
   mutate(y = c(.7, .8, .7, .8))
 tbl_main_seq <- crossing(History = v_history, ID = v_ID) %>%
@@ -377,10 +440,10 @@ tbl_main_seq <- crossing(History = v_history, ID = v_ID) %>%
 tbl_idio_seq <- crossing(History = v_history, ID = v_ID) %>%
   mutate(y = c(.7, .7, .7, .8))
 
-plt_seq_only <- plot_example_conditions(tbl_main_seq, "Only Sequence Effect", plot_legend = TRUE)
-plt_id_only <- plot_example_conditions(tbl_main_id, "Only Ind. Differences")
-plt_idio_seq <- plot_example_conditions(tbl_idio_seq, "Sequence Effect By ID")
+#plt_seq_only <- plot_example_conditions(tbl_main_seq, "Only Sequence Effect", plot_legend = TRUE)
+plt_id_only <- plot_example_conditions(tbl_main_id, "Individual Differences")
+plt_idio_seq <- plot_example_conditions(tbl_idio_seq, "Idiosyncratic Seq. Effect")
 
-grid.draw(arrangeGrob(plt_seq_only, plt_id_only, plt_idio_seq, nrow = 1))
+grid.draw(arrangeGrob(plt_id_only, plt_idio_seq, nrow = 1))
 
 
