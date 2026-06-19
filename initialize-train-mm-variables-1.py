a07a98c2b26081568ff710c62a77879136b97605
MirkoTh <mirkothalmann@hotmail.com>
2026-06-19 11:34:01 +0200

    update loading of wandb results

--- initialize-train-mm-variables-1.py
+++ initialize-train-mm-variables-1.py
@@ -136,8 +136,8 @@ def run_command(args):
 
 
 # serial
-# for args in arg_combinations:
-#     run_command(args)
+for args in arg_combinations:
+    run_command(args)
 
 # parallel
 # with ThreadPoolExecutor(max_workers=20) as executor:
