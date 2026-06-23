c4513284bd94a0609315fd8c229f6f377e23cd8a
MirkoTh <mirkothalmann@hotmail.com>
2026-06-23 09:46:19 +0200

    use mamba for conda environment

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ os.makedirs("data", exist_ok=True)
 with tarfile.open("data/SharedResponses.csv.tar.gz", "r:gz") as tar:
     tar.extractall(path="data", filter="data")
 
+
+# install mamba for memory efficiency
+subprocess.run(["conda", "install", "-n", "base", "-c", "conda-forge", "mamba"], check=True)
 # Create conda environment
-subprocess.run(["conda", "env", "create", "-f", "SoV.yaml"], check=True)
+subprocess.run(["mamba", "env", "create", "-f", "SoV.yaml"], check=True)
